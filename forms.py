067e4f0e93e647cbd3aed0429434953dab3789f7
FancyFar <155945144+FancyFar@users.noreply.github.com>
2024-01-16 13:38:27 +0800

    Update forms.py

--- forms.py
+++ forms.py
@@ -23,8 +23,16 @@ class CreateProductForm(Form):
     drinks = StringField('Drinks', [validators.Length(min=1, max=150), validators.DataRequired()])
 
 
+from wtforms import Form, StringField, RadioField, SelectField, TextAreaField, validators, EmailField, DateField
+
+
 class CreateQuestionForm(Form):  # inherit from Form
     title = StringField('Title',[validators.Length(min=1, max=150), validators.DataRequired()])
-    email = EmailField('Email', [validators.Email(), validators.DataRequired()])
+    email = EmailField('Email', [validators.Email(), validators.Optional()])
     date_posted = DateField('Date Posted', format='%Y-%m-%d')
-    question = TextAreaField('Question(s)', [validators.DataRequired()])
+    question = TextAreaField('Question(s) (if any)', [validators.DataRequired()])
+    overall = RadioField('Overall Experience', choices=[('B', 'Bad'), ('N', 'Neutral'), ('E', 'Excellent')],
+                     default='N')
+    feedback = TextAreaField('Feedback (if any)', [validators.Optional()])
+
+

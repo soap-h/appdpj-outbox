5f320a667a6a0d44c1e804a779a10026a8207386
FancyFar <155945144+FancyFar@users.noreply.github.com>
2024-01-17 18:04:12 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -365,7 +365,7 @@ def retrieve_questions():
 @app.route('/cviewQuestion')
 def cretrieve_questions():
     questions_dict = {}
-    db = shelve.open('question.db', 'r')
+    db = shelve.open('database.db', 'r')
     questions_dict = db['Question']
     db.close()
     questions_list = []

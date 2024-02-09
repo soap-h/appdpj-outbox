3c79e0b8d119ffe9c4121adab2b46fb191895538
FancyFar <155945144+FancyFar@users.noreply.github.com>
2024-02-10 00:41:19 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -806,7 +806,42 @@ def retrieve_questions():
         questions_list.append(question)
     return render_template('retrieveQuestion.html', count=len(questions_list), questions_list=questions_list)
 
+@app.route('/filter_questions', methods=['GET', 'POST'])
+def filter_questions(value=None):
+    global questions_list
+    value = request.args.get('filter_type') or request.form.get('filter_type')
+    if value == 'all':
+        db = shelve.open('database.db', 'r')
+        questions_dict = db['Question']
+        db.close()
+        questions_list = [question for question in questions_dict.values()]
 
+    elif value == 'question':  # have question attribute
+        db = shelve.open('database.db', 'r')
+        questions_dict = db['Question']
+        db.close()
+        questions_list = [question for question in questions_dict.values() if question.get_question() != ""]
+
+    elif value == "feedback":  # have feedback attribute
+        db = shelve.open('database.db', 'r')
+        questions_dict = db['Question']
+        db.close()
+        questions_list = [question for question in questions_dict.values() if question.get_feedback() != ""]
+
+    elif value == "reply":  # have reply attribute
+        db = shelve.open('database.db', 'r')
+        questions_dict = db['Question']
+        db.close()
+        questions_list = [question for question in questions_dict.values() if question.get_reply() != ""]
+
+    elif value == "no_reply":  # have reply attribute
+        db = shelve.open('database.db', 'r')
+        questions_dict = db['Question']
+        db.close()
+        questions_list = [question for question in questions_dict.values() if question.get_reply() == ""]
+
+    return render_template('retrieveQuestion.html', count=len(questions_list), questions_list=questions_list)
+    
 @app.route('/cviewQuestion')
 def cretrieve_questions():
     questions_dict = {}
